65ebe24a9fcff1867a2adb0035c726106bd20dfd
llandau <leo_landau@yahoo.com>
2022-03-11 10:25:26 -0800

    view to show number of drug labels in the db

--- dle/data/views.py
+++ dle/data/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import DrugLabel
 
-# Create your views here.
+def index(request):
+    num_drug_labels = DrugLabel.objects.count()
+    str = f"There are {num_drug_labels} Drug Labels in the system"
+    return HttpResponse(str)
