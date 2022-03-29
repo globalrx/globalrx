d180c9acc2034a582ff88750dfcb1f87d11224fb
Ky <ky@MacBook-Pro-2.local>
2022-03-29 22:56:34 +0200

    saving progress

--- dle/search/views.py
+++ dle/search/views.py
@@ -1,44 +1,24 @@
 from functools import reduce
-from django.http import (
-    HttpRequest,
-    HttpResponse
-)
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from .search_mock_utils import SEARCH_RESULTS
+from .search_constants import TYPE_AHEAD_MAPPING
 from . import services as SearchService
-
-type_ahead_mapping = {
-    "include_manufacturer": [
-        "bayer",
-        "gsk",
-        "pfizer",
-    ],
-    "include_generic_name": [
-        "hola",
-        "como",
-        "stas",
-    ],
-    "include_active_ingredients": [
-        "bcaa",
-        "creatine",
-        "acetaminophen",
-    ]
-}
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    """Landing page search view.
-    """
-    context = {}
-    if request.htmx:
-        query = request.GET
-        if query:
-            type_ahead = [
-                type_ahead_mapping[k] for k in query.keys()
-            ]
-            context["type_ahead_datalist"] = reduce(lambda acc,e: acc + e, type_ahead, [])
-            print(context)
-    return render(request, "search/search.html", context)
+    """Landing page search view."""
+    context = {
+        "type_ahead_manufacturer": TYPE_AHEAD_MAPPING["manufacturers"],
+        "type_ahead_marketing_category": TYPE_AHEAD_MAPPING["marketing_category"],
+        "type_ahead_country": TYPE_AHEAD_MAPPING["country"],
+        "type_ahead_generic_name": TYPE_AHEAD_MAPPING["generic_name"],
+        "type_ahead_brand_name": TYPE_AHEAD_MAPPING["brand_name"],
+        "type_ahead_ndc": TYPE_AHEAD_MAPPING["ndc"],
+    }
+
+    return render(request, "search/search_landing/search_landing.html", context)
+
 
 def list_search_results(request: HttpRequest) -> HttpResponse:
     """Search results list view
@@ -51,10 +31,8 @@ def list_search_results(request: HttpRequest) -> HttpResponse:
     """
     search_request_object = SearchService.validate_search(request.GET)
     search_results = [
-        SearchService.build_search_result(result,search_request_object.search_text)
+        SearchService.build_search_result(result, search_request_object.search_text)
         for result in SEARCH_RESULTS
     ]
-    context = {
-        'search_results': search_results
-    }
-    return render(request, "search/search_results.html", context=context)
+    context = {"search_results": search_results}
+    return render(request, "search/search_results/search_results.html", context=context)
