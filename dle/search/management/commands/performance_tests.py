551af7e187d6663bb8694e87a9bc5e79a66f9a85
llandau <leo_landau@yahoo.com>
2022-04-12 14:47:25 -0700

    make a plot for the performance tests

--- dle/search/management/commands/performance_tests.py
+++ dle/search/management/commands/performance_tests.py
@@ -9,6 +9,9 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+PERF_TEST_CSV = "perf_test.csv"
+PERF_TEST_PNG = "perf_test.png"
+
 TEST_QUERIES = [
     # select_section=&search_text=&select_agency=&manufacturer_input=&generic_name_input=&brand_name_input=
     {
@@ -27,13 +30,79 @@ TEST_QUERIES = [
         "generic_name_input": "",
         "brand_name_input": "",
     },
+    {
+        "select_section": "indications",
+        "search_text": '"heart disease"',
+        "select_agency": "",
+        "manufacturer_input": "",
+        "generic_name_input": "",
+        "brand_name_input": "",
+    },
+    {
+        "select_section": "",
+        "search_text": '"heart disease"',
+        "select_agency": "",
+        "manufacturer_input": "",
+        "generic_name_input": "",
+        "brand_name_input": "",
+    },
+    {
+        "select_section": "indications",
+        "search_text": '"heart disease"',
+        "select_agency": "",
+        "manufacturer_input": "",
+        "generic_name_input": "",
+        "brand_name_input": "",
+    },
+    {
+        "select_section": "indications",
+        "search_text": '"heart disease"',
+        "select_agency": "FDA",
+        "manufacturer_input": "",
+        "generic_name_input": "",
+        "brand_name_input": "",
+    },
+    {
+        "select_section": "indications",
+        "search_text": '"heart disease"',
+        "select_agency": "EMA",
+        "manufacturer_input": "",
+        "generic_name_input": "",
+        "brand_name_input": "",
+    },
+    {
+        "select_section": "indications",
+        "search_text": '"heart disease"',
+        "select_agency": "FDA",
+        "manufacturer_input": "",
+        "generic_name_input": "ibuprofen",
+        "brand_name_input": "",
+    },
+    {
+        "select_section": "indications",
+        "search_text": '"heart disease"',
+        "select_agency": "FDA",
+        "manufacturer_input": "pfizer",
+        "generic_name_input": "ibuprofen",
+        "brand_name_input": "",
+    },
+    {
+        "select_section": "indications",
+        "search_text": '"heart disease"',
+        "select_agency": "FDA",
+        "manufacturer_input": "pfizer",
+        "generic_name_input": "ibuprofen",
+        "brand_name_input": "latuda",
+    },
 ]
 
-# e.g. `python manage.py performance_tests --verbosity 2 --num_runs 4`
 # runs with `python manage.py performance_tests`
-# add `--num_runs 3` to run the tests 3 times
-# add `--verbosity 2` for info output
-# add `--verbosity 3` for debug output
+# e.g. `python manage.py performance_tests --verbosity 2 --num_runs 2`
+# `--num_runs X` sets the number of runs
+# `--verbosity 2` gives info output
+# `--verbosity 3` gives debug output
+# `--skip_tests True` skips the tests, so we can just do the plot
+# `--make_plot False` skips making the plot
 class Command(BaseCommand):
     help = "Runs performance tests, outputs results to media/perf_test.csv"
 
@@ -45,6 +114,15 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
             "--num_runs", type=int, help="number of test runs", default=2
+        )
+        parser.add_argument(
+            "--make_plot", type=bool, help="set to True to make plot", default=True
+        )
+        parser.add_argument(
+            "--skip_tests",
+            type=bool,
+            help="set to True to skip the testing",
+            default=False,
         )
 
     def set_log_verbosity(self, verbosity):
@@ -62,11 +140,22 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         num_runs = options["num_runs"]
-
+        make_plot = options["make_plot"]
+        skip_tests = options["skip_tests"]
         self.set_log_verbosity(int(options["verbosity"]))
-
         logger.info(self.style.SUCCESS("start process"))
 
+        if not skip_tests:
+            self.run_tests(num_runs)
+
+        # make_plot is not going to run the tests
+        # will use perf_test.csv from previous run(s)
+        if make_plot:
+            self.make_plot()
+
+        logger.info(self.style.SUCCESS("process complete"))
+
+    def run_tests(self, num_runs):
         # we want to run a number of performance tests
         # output response times to a file
         # create a set of test queries
@@ -75,7 +164,7 @@ class Command(BaseCommand):
         # print a file to output the times in csv
         # query_0, query1, query2
 
-        output_file = settings.MEDIA_ROOT / "perf_test.csv"
+        output_file = settings.MEDIA_ROOT / PERF_TEST_CSV
         f = default_storage.open(output_file, "a")
         logger.info(f"saving data to: {output_file}")
 
@@ -105,3 +194,43 @@ class Command(BaseCommand):
 
             query_time_csv_str = ",".join(query_times) + "\n"
             f.write(query_time_csv_str)
+
+    def make_plot(self):
+        logger.info("running make_plot")
+        file = settings.MEDIA_ROOT / PERF_TEST_CSV
+        import pandas as pd
+        import matplotlib.pyplot as plt
+
+        df = pd.read_csv(file, header=None)
+        num_rows = df.shape[0]
+        num_cols = df.shape[1]
+
+        x = []
+        y = []
+        x_avg = []
+        y_avg = []
+        # skip the first column
+        for col in range(1, num_cols):
+            logger.info(f"average query time; col: {col}; value: {df[col].mean()}")
+            x_avg.append(col)
+            y_avg.append(df[col].mean())
+
+            for row in range(num_rows):
+                logger.debug(f"row: {row}; col: {col}; value: {df[col][row]}")
+                x.append(col)
+                y.append(df[col][row])
+
+        total_avg = sum(y_avg) / len(y_avg)
+        logger.info(f"total average query time: {total_avg}")
+
+        plt.figure(figsize=(16, 6))
+        plt.scatter(x, y, label="query time")
+        plt.scatter(x_avg, y_avg, label="avg query time per query")
+        plt.axhline(total_avg, label="avg query time")
+        plt.title("Performance testing")
+        plt.xlabel("Queries")
+        plt.ylabel("time to complete query (seconds)")
+        plt.legend(loc="best")
+
+        fig_file = settings.MEDIA_ROOT / PERF_TEST_PNG
+        plt.savefig(fig_file)
