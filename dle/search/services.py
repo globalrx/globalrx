bb3ee8ca011c190ff5ffedbbf5ace81212c4db47
llandau <leo_landau@yahoo.com>
2022-04-17 15:13:46 -0700

    revert changes to services, views

--- dle/search/services.py
+++ dle/search/services.py
@@ -2,113 +2,88 @@ from typing import List, Tuple, Dict
 from .models import SearchRequest, InvalidSearchRequest
 from .search_constants import MAX_LENGTH_SEARCH_RESULT_DISPLAY
 from data.models import DrugLabel, ProductSection
-from django.http import QueryDict
-from django.db.models import Count
-from django.db import connection
-import logging
 
-logger = logging.getLogger(__name__)
+from django.http import QueryDict
+
 
 def validate_search(request_query_params_dict: QueryDict) -> SearchRequest:
-    """Validates search params and returns the search request object if valid.
-
+    """Validates search params and returns the seach request object if valid.
     Args:
         request_query_params_dict (QueryDict): Request dictionary returned from HttpRequest.GET
-
     Raises:
         InvalidSearchRequest
-
     Returns:
         SearchRequest: Validated search tuple object
     """
     search_request_object = SearchRequest.from_search_query_dict(
         request_query_params_dict
     )
-    logger.debug(f"search_request_object: {search_request_object}")
 
     if search_request_object.search_text is not None:
         return search_request_object
     else:
         raise InvalidSearchRequest("Search request is malformed")
 
+def build_match_query(search_query: str) -> str:
+    return """
+        AND match(ps.section_text) AGAINST (
+                %(search_query)s IN NATURAL LANGUAGE MODE
+            )    
+           """
 
-def dl_query(search_request: SearchRequest) -> List[ProductSection]:
+
+def process_search(search_request: SearchRequest) -> List[DrugLabel]:
     search_filter_mapping = {
         "select_section": "section_name",
         "select_agency": "source",
         "manufacturer_input": "marketer",
-        "marketing_category_input": "marketer",
-        "generic_name_input": "generic_name",
-        "brand_name_input": "product_name",
-    }
-    sql_params = {"search_text": search_request.search_text}
-    sql = """
-        SELECT id
-        FROM data_productsection
-        WHERE
-            match(section_text) AGAINST (
-                %(search_text)s IN NATURAL LANGUAGE MODE
-            )
-    """
-    sql += " AND "
-
-    sql = """
-    SELECT count(*)
-    FROM data_druglabel
-    """
-    sql_params = {}
-    with connection.cursor() as cursor:
-        cursor.execute(sql, sql_params)
-        result = cursor.fetchall()
-
-    # get the matching drug labels
-    # get the sections where
-
-    return result
-
-def process_search(search_request: SearchRequest) -> List[ProductSection]:
-    search_filter_mapping = {
-        "select_section": "section_name",
-        "select_agency": "source",
-        "manufacturer_input": "marketer",
-        "marketing_category_input": "marketer",
         "generic_name_input": "generic_name",
         "brand_name_input": "product_name",
     }
     search_request_dict = search_request._asdict()
-    sql_params = {"search_text": search_request.search_text}
-
+    search_query = search_request.search_text.strip('"').lower()
     raw_sql = """
-        SELECT ps.*
-        FROM data_productsection AS ps
-        JOIN data_labelproduct as lp ON lp.id = ps.label_product_id
-        JOIN data_druglabeldoc as dld ON dld.id = lp.drug_label_id
-        JOIN data_druglabel as dl ON dld.label_id = dl.label_id
+        SELECT
+            DISTINCT dl.id,
+            dl.source,
+            dl.product_name,
+            dl.generic_name,
+            dl.version_date,
+            dl.source_product_number,
+            dl.marketer,
+            dl.link
+        FROM
+            data_druglabel as dl
+        JOIN data_labelproduct as lp ON dl.id = lp.drug_label_id
+        JOIN data_productsection as ps ON lp.id = ps.label_product_id
         WHERE
-            section_text = %(search_text)s
-    """
-    # section_text LIKE '%% %(search_text)s %%'
-    # TODO look into: Function 'match' isn't supported ...?
+            1=1
+        """
 
+    # exact match query
+    if '"' in search_request.search_text:
+        raw_sql += " AND LOWER(ps.section_text) LIKE CONCAT('%%',%(search_query)s,'%%') "
+    else:
+        raw_sql += build_match_query(search_query)
+
+    sql_params = {"search_query": search_query}
     for k, v in search_request_dict.items():
         if v and k in search_filter_mapping:
             param_key = search_filter_mapping[k]
+            if k == "select_section" and not v:
+                continue
             sql_params[param_key] = v
-            additional_filter = f"AND LOWER({param_key}) = LOWER(%({param_key})s) "
+            additional_filter = f"AND LOWER({param_key}) = %({param_key})s "
             raw_sql += additional_filter
-    raw_sql += "LIMIT 30"  # can remove this once we're done testing
-    logger.debug(raw_sql)
-
-    return [ps for ps in ProductSection.objects.raw(raw_sql, params=sql_params)]
+    raw_sql += "LIMIT 30" #can remove this once we're done testing
+    return [d for d in DrugLabel.objects.raw(raw_sql, params=sql_params)]
 
 
 def highlight_text_by_term(text: str, search_term: str) -> Tuple[str, bool]:
     """Builds the highlighted texted for a given string.
-
     Args:
         text (str): Original text to highlight
         search_term (str): Term that should be highlighted within the text string.
-
     Returns:
         Tuple[str, bool]: The Highlighted Text and True if highlighting is successful
     """
@@ -137,70 +112,40 @@ def highlight_text_by_term(text: str, search_term: str) -> Tuple[str, bool]:
     return " ".join(tokens), highlighted
 
 
-def highlight_search_text(ps: ProductSection, search_term: str) -> str:
-    """Returns highlighted text, where the search_term is found in the text
-
+def build_search_result(
+    search_result: DrugLabel, search_term: str
+) -> Tuple[DrugLabel, str]:
+    """Returns search result objects with highlighted text
     Args:
-        ps (ProductSection): The ProductSection from the DrugLabel
+        search_result (MockDrugLabel): A fake label that is used until we have a dataset
         search_term (str): The search text to highlight
-
     Returns:
-        str: highlighted text
+        Tuple[MockDrugLabel, str]: Tuple object with the full drug label object and a truncated version of its text
     """
     start, end, step = (
         0,
-        len(ps.section_text),
+        len(search_result.generic_name), #TODO CHANGE THIS WHEN FIXING HIGHLIGHTING
         MAX_LENGTH_SEARCH_RESULT_DISPLAY,
     )
     # sliding window approach to mimic google's truncation
     for i in range(start, end, step):
-        text = ps.section_text[i : i + step]
+        text = search_result.generic_name[i : i + step]
         highlighted_text, did_highlight = highlight_text_by_term(text, search_term)
         if did_highlight:
-            return highlighted_text
+            return search_result, highlighted_text
 
-    return ps.section_text[start:step]
-
-
-def map_custom_names_to_section_names(name_list: List[str]) -> List[str]:
-    SECTION_NAME_MAPPING = {
-        "('contra', 'contraindications')": "contra",
-        "('drive', 'effects on driving')": "drive",
-        "('indications', 'indications')": "indications",
-        "('interact', 'interactions')": "interact",
-        "('over', 'overdose')": "over",
-        "('pose', 'posology')": "pose",
-        "('preg', 'pregnancy')": "preg",
-        "('side', 'side effects')": "side",
-        "('warn', 'warnings')": "warn",
-    }
-    return list(
-        {
-            n if n not in SECTION_NAME_MAPPING else SECTION_NAME_MAPPING[n]
-            for n in name_list
-        }
-    )
+    return search_result, search_result.generic_name[start:step]
 
 
 def get_type_ahead_mapping() -> Dict[str, str]:
-    values = DrugLabel.objects.values('marketer').annotate(count=Count('marketer')).order_by('-count')[:20]
-    marketers = [v["marketer"] for v in values]
-
-    values = DrugLabel.objects.values('generic_name').annotate(count=Count('generic_name')).order_by('-count')[:20]
-    generic_names = [v["generic_name"] for v in values]
-
-    values = DrugLabel.objects.values('product_name').annotate(count=Count('product_name')).order_by('-count')[:20]
-    product_names = [v["product_name"] for v in values]
-
-    values = ProductSection.objects.values('section_name').annotate(count=Count('section_name')).order_by('-count')[:20]
-    section_names = [v["section_name"] for v in values]
-
-    logger.debug([s.lower() for s in section_names])
+    marketers = DrugLabel.objects.values_list("marketer", flat=True).distinct()
+    generic_names = DrugLabel.objects.values_list("generic_name", flat=True).distinct()
+    product_names = DrugLabel.objects.values_list("product_name", flat=True).distinct()
     type_ahead_mapping = {
-        "manufacturers": marketers,
-        "generic_name": generic_names,
-        "brand_name": product_names,
-        "section_name": section_names,
+        "manufacturers": [m.lower() for m in marketers],
+        "generic_name": [g.lower() for g in generic_names],
+        "brand_name": [p.lower() for p in product_names],
+        "section_name": ["warnings", "pregnancy", "indications", "overdose", "interact", "side effects"], # hardcode this until figure out subsections
     }
 
     return type_ahead_mapping
--- dle/search/views.py
+++ dle/search/views.py
@@ -21,20 +21,17 @@ def index(request: HttpRequest) -> HttpResponse:
 
 def list_search_results(request: HttpRequest) -> HttpResponse:
     """Search results list view
-
     Args:
         request (HttpRequest): GET request with search text and multiple boolean flags.
-
     Returns:
         HttpResponse: Search results view with highlighted text
     """
     search_request_object = SearchService.validate_search(request.GET)
     results = SearchService.process_search(search_request_object)
-    search_results = []
-    for ps in results:
-        highlighted_text = SearchService.highlight_search_text(ps, search_request_object.search_text)
-        dl = ps.label_product.drug_label
-        search_results.append((dl, highlighted_text))
+    search_results = [
+        SearchService.build_search_result(result, search_request_object.search_text)
+        for result in results
+    ]
 
     context = {"search_results": search_results}
     return render(request, "search/search_results/search_results.html", context=context)
