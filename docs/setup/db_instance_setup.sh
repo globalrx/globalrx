d022077542f0ba4df58da5b011f62d5d6714b9e4
llandau <leo_landau@yahoo.com>
2022-04-17 15:18:10 -0700

    remove columnstore from db instance

--- docs/setup/db_instance_setup.sh
+++ docs/setup/db_instance_setup.sh
@@ -20,8 +20,7 @@ sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] https://mirrors.xtom.com
 
 sudo apt update
 sudo apt install libjemalloc1 -y
-sudo apt install mariadb-server mariadb-backup libmariadb3 \
-  mariadb-client mariadb-plugin-columnstore -y
+sudo apt install mariadb-server mariadb-backup libmariadb3 mariadb-client -y
 
 ##### Setup a custom config file
 
@@ -47,34 +46,6 @@ EOF
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
 
-sudo systemctl start mariadb-columnstore
-sudo systemctl enable mariadb-columnstore
-
-##### Optimize Linux Kernel Paramaters
-
-sudo tee /etc/sysctl.d/90-mariadb-columnstore.conf > /dev/null <<EOF
-# minimize swapping
-vm.swappiness = 1
-
-# Increase the TCP max buffer size
-net.core.rmem_max = 16777216
-net.core.wmem_max = 16777216
-
-# Increase the TCP buffer limits
-# min, default, and max number of bytes to use
-net.ipv4.tcp_rmem = 4096 87380 16777216
-net.ipv4.tcp_wmem = 4096 65536 16777216
-
-# don't cache ssthresh from previous connection
-net.ipv4.tcp_no_metrics_save = 1
-
-# for 1 GigE, increase this to 2500
-# for 10 GigE, increase this to 30000
-net.core.netdev_max_backlog = 2500
-EOF
-
-sudo sysctl --load=/etc/sysctl.d/90-mariadb-columnstore.conf
-
 ##### Set the locale
 
 sudo localedef -i en_US -f UTF-8 en_US.UTF-8
@@ -92,24 +63,8 @@ GRANT ALL PRIVILEGES ON test_$DB_NAME.* TO '$DB_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-# setup the cross_engine user
-sudo mysql <<EOF
-CREATE USER 'cross_engine'@'127.0.0.1' IDENTIFIED BY "cross_engine_passwd";
-CREATE USER 'cross_engine'@'localhost' IDENTIFIED BY "cross_engine_passwd";
-GRANT SELECT, PROCESS ON *.* TO 'cross_engine'@'127.0.0.1';
-GRANT SELECT, PROCESS ON *.* TO 'cross_engine'@'localhost';
-EOF
-
-##### Cross Engine Support
-
-sudo mcsSetConfig CrossEngineSupport Host 127.0.0.1
-sudo mcsSetConfig CrossEngineSupport Port 3306
-sudo mcsSetConfig CrossEngineSupport User cross_engine
-sudo mcsSetConfig CrossEngineSupport Password cross_engine_passwd
-
 ##### Restart the services
 
 sudo systemctl restart mariadb
-sudo systemctl restart mariadb-columnstore
 
 #####
