625af75f17232533dc0f3567bca715315f0eff27
llandau <leo_landau@yahoo.com>
2022-04-21 11:58:12 -0700

    testing CI changes

--- dle/dle/settings.py
+++ dle/dle/settings.py
@@ -103,6 +103,9 @@ DATABASES = {
     }
 }
 
+# override host for CI process
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES["default"]["HOST"] = "127.0.0.1"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
