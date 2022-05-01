f46a9a2c8e24868f23bc262c7828eee2ba6c85b3
Sam Negassi <samson.s.negassi@gmail.com>
2022-05-01 13:24:10 -0700

    dropdown to jump to a specific section

--- dle/data/views.py
+++ dle/data/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import DrugLabel, LabelProduct, ProductSection
 from django.core.exceptions import ObjectDoesNotExist
+from compare.util import *
 
 
 def index(request):
@@ -16,19 +17,38 @@ def single_label_view(request, drug_label_id):
     try:
         label_product = LabelProduct.objects.filter(drug_label_id=drug_label_id).get()
         product_sections = ProductSection.objects.filter(label_product_id=label_product.id).all()
+        sections_dict = {}
+        section_names = []
         for section in product_sections:
-            print(f"section_name: {section.section_name}")
+            section_names.append(section.section_name)
+            sections_dict[section.section_name] = {"section_name": section.section_name}
+            
             if drug_label.source == "EMA":
-                section.section_text = section.section_text.replace("\n", "<br>")
+                sections_dict[section.section_name]["section_text"] = section.section_text.replace("\n", "<br>")
+            else:
+                sections_dict[section.section_name]["section_text"] = section.section_text
+
     except ObjectDoesNotExist:
-        product_sections = []
+        sections_dict = {}
+        section_names = []
 
     # get all drug labels with the same product_name
     drug_label_versions = DrugLabel.objects.filter(product_name=drug_label.product_name).order_by('-version_date')
+    section_names.sort()
 
     context = {
         "drug_label": drug_label,
-        "product_sections": product_sections,
         "drug_label_versions": drug_label_versions,
+        "section_names": section_names,
+        "sections": []
     }
+    
+    for sec_name in SECTIONS_ORDER:
+        if sec_name in sections_dict.keys():
+            context["sections"].append(sections_dict[sec_name])
+    
+    for key, val in sections_dict.items():
+        if key not in SECTIONS_ORDER:
+            context["sections"].append(val)
+
     return render(request, "data/single_label.html", context)
