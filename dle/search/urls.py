949fe20ae6171eb68540e7547599c8ec29729b56
llandau <leo_landau@yahoo.com>
2022-05-02 20:32:09 -0700

    update style from ll/my_labels_3 / nlp1

--- dle/search/urls.py
+++ dle/search/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "search"
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("results", views.list_search_results, name="list_search_results"),
-    path("<int:drug_id>", views.view_drug, name="view_drug")
 ]
