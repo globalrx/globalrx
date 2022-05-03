ed52a2cbcf69b9879637182860dac1eb74f44f68
Sam Negassi <samson.s.negassi@gmail.com>
2022-05-03 00:38:00 -0700

    section text formatting in single label view

--- dle/data/views.py
+++ dle/data/views.py
@@ -35,6 +35,17 @@ def single_label_view(request, drug_label_id, search_text=""):
                             text += f"{word} "
             else:
                 text = section.section_text
+
+            # convert common xml tags to html tags
+            text = text.replace('<list listtype="unordered" ', '<ul ')
+            text = text.replace('<list', '<ul ')
+            text = text.replace('</list>', '</ul>')
+            text = text.replace('<item', '<li')
+            text = text.replace('</item>', '</li>')
+            text = text.replace('<paragraph', '<p')
+            text = text.replace('</paragraph>', '</p>')
+            text = text.replace('<linkhtml', '<a')
+            text = text.replace('</linkhtml>', '</a>')
             
             if drug_label.source == "EMA":
                 sections_dict[section.section_name]["section_text"] = text.replace("\n", "<br>")
