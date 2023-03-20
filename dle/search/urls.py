0b872181d7570ecfca9f5957cb63015f7a4ef76a
Cole Crawford <16374762+ColeDCrawford@users.noreply.github.com>
2023-03-20 00:30:50 -0400

    Test SearchKit frontend template

--- dle/search/urls.py
+++ dle/search/urls.py
@@ -7,4 +7,5 @@ app_name = "search"
 urlpatterns = [
     path("", views.index, name="index"),
     path("results", views.list_search_results, name="list_search_results"),
+    path("es_search", views.es_search, name="es_search"),
 ]
