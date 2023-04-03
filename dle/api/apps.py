665c362c3513848c67cef9b5a36423c93c3e1792
Cole Crawford <16374762+ColeDCrawford@users.noreply.github.com>
2023-04-03 01:09:41 -0400

    Move BERT model to api.apps.py so we can reload it
    Makes app startup slower, but vectorization route much faster

--- dle/api/apps.py
+++ dle/api/apps.py
@@ -1,6 +1,15 @@
+import os
+
 from django.apps import AppConfig
+from django.conf import settings
+
+from sentence_transformers import SentenceTransformer
 
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+    # TODO script this startup so it doesn't download multiple times
+    # pubmedbert_model = SentenceTransformer("pritamdeka/S-PubMedBert-MS-MARCO")
+    MODEL_PATH = os.path.join(settings.NLP_MODELS, "S-PubMedBert-MS-MARCO")
+    pubmedbert_model = SentenceTransformer(MODEL_PATH)
