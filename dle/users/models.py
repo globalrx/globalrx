a1ff346414ce1bf15ed70355375c50e3ca872568
Robert Cavezza <rcavezza@gmail.com>
2023-04-05 22:08:43 -0500

    Saved searches in progress

--- dle/users/models.py
+++ dle/users/models.py
@@ -25,3 +25,9 @@ class MyLabel(models.Model):
             f"file.name: {self.file.name}, "
             f"is_successfully_parsed: {self.is_successfully_parsed}"
         )
+
+class SavedSearch(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    url = models.TextField()
+    name = models.CharField(max_length=255, db_index=True)
+
--- dle/users/urls.py
+++ dle/users/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("my_labels/", views.my_labels_view, name="my_labels"),
     path("my_labels/create/", views.create_my_label, name="create_my_label"),
+    # path("saved_searches/", views.saved_searches_view, name="saved_searches"),
+    # path("saved_searches/create/", views.create_saved_search_view, name="create_my_saved_search"),
 ]
