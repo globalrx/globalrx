8db090ddac4f71d1f05dd25a3706dcdb139e16bb
Cole Crawford <16374762+ColeDCrawford@users.noreply.github.com>
2023-04-06 12:04:38 -0400

    Move model preload logic from load_bert_model mgmt command to app config

--- dle/api/apps.py
+++ dle/api/apps.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from django.apps import AppConfig
@@ -6,6 +7,8 @@ from django.conf import settings
 from sentence_transformers import SentenceTransformer
 
 
+logger = logging.getLogger(__name__)
+
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
@@ -13,5 +16,17 @@ class ApiConfig(AppConfig):
     if settings.TESTS==True:
         pubmedbert_model = SentenceTransformer("pritamdeka/S-PubMedBert-MS-MARCO")
     else:
-        MODEL_PATH = os.path.join(settings.NLP_MODELS, "S-PubMedBert-MS-MARCO")
+        # TODO allow for some configuration rather than hardcoding S-PubMedBert
+        model_repo = "pritamdeka/S-PubMedBert-MS-MARCO"
+        username, repo = [str(i) for i in model_repo.split("/")]
+        MODEL_PATH = os.path.join(settings.NLP_MODELS, repo)
+        if os.path.exists(MODEL_PATH):
+            print(f"{model_repo} already exists at {MODEL_PATH}")
+            logger.info(f"{model_repo} already exists at {MODEL_PATH}")
+        else:
+            print("Model doesn't exist, downloading")
+            os.makedirs(MODEL_PATH)
+            model = SentenceTransformer(model_repo)
+            model.save(MODEL_PATH)
+            logger.info(f"Saved {model_repo} to {MODEL_PATH}")
         pubmedbert_model = SentenceTransformer(MODEL_PATH)
