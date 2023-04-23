298dc612fd3caba00eee8975d1955a9979d01a6e
Cole Crawford <16374762+ColeDCrawford@users.noreply.github.com>
2023-04-23 17:45:43 -0400

    Remove unused import for linting

--- dle/search/views.py
+++ dle/search/views.py
@@ -1,6 +1,5 @@
 from typing import List, Set, Tuple
 
-from django.conf import settings
 from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
