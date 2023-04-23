8e99910fe5ec656757f4bb87b9c8cb4f54cc0cd9
Cole Crawford <16374762+ColeDCrawford@users.noreply.github.com>
2023-04-23 16:36:02 -0400

    Remove matching pre-computed vectors to existing Django data
    Didn't work well

--- dle/data/management/commands/vectorize.py
+++ dle/data/management/commands/vectorize.py
@@ -2,11 +2,8 @@ import asyncio
 import json
 import logging
 from datetime import datetime
-from distutils.util import strtobool
 
 from django.core.management.base import BaseCommand, CommandError
-
-from tqdm import tqdm
 
 from api.apps import ApiConfig
 from data.models import ProductSection
@@ -38,17 +35,6 @@ class Command(BaseCommand):
             type=str,
             help="'TGA', 'FDA', 'EMA', 'HC', or 'all'",
         )
-        parser.add_argument(
-            "--vectorize",
-            type=strtobool,
-            help="Vectorizes existing data for the given agency",
-            default=True,
-        )
-        parser.add_argument(
-            "--vector_file",
-            type=str,
-            help="If a file is passed, insert the existing vectors into Django given existing (unvectorized) agency data",
-        )
 
     @background
     def compute_section_vector_wrapper(self, section):
@@ -58,8 +44,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         agency = options["agency"]
-        vectorize = options["vectorize"]
-        vector_filename = options["vector_file"]
 
         if agency not in ["EMA", "FMA", "TGA", "HC", "all"]:
             raise CommandError("'agency' parameter must be an agency")
@@ -67,56 +51,20 @@ class Command(BaseCommand):
         logger.info(self.style.SUCCESS("start vectorizing"))
         logger.info(f"Agency: {agency}")
 
-        # TODO finish and test
-        if vector_filename:
-            """Load the vectors from file to PSQL via Django ORM"""
-            logger.info(f"Opening {vector_filename}")
-            with open(vector_filename, "r") as f:
-                vectors = json.load(f)
-            logger.info("Vectors JSON loaded. Ingesting vectors into Django.")
-            for source_product_id in tqdm(vectors.keys()):
-                versions = vectors[source_product_id]
-                for version in versions.keys():
-                    # logger.info(f"{source_product_id} - {version}")
-                    for section_name in vectors[source_product_id][version]:
-                        # logger.info(f"{source_product_id} - {version} - {section_name}")
-                        vector = json.loads(vectors[source_product_id][version][section_name])
-                        try:
-                            section = ProductSection.objects.get(
-                                section_name=section_name,
-                                label_product__drug_label__source_product_number=source_product_id,
-                                label_product__drug_label__version_date=datetime.strptime(
-                                    version, "%Y/%m/%d"
-                                ),
-                            )
-                            section.bert_vector = vector
-                            section.save()
-                        except ProductSection.DoesNotExist:
-                            logger.error(
-                                f"Could not find ProductSection based on: section_name={section_name}, label_product__drug_label__source_product_number={source_product_id}, and label_product__drug_label__version_date={datetime.strptime(version, '%Y/%m/%d')} "
-                            )
-                        except ProductSection.MultipleObjectsReturned:
-                            logger.error(
-                                f"Found multiple ProductSections based on: section_name={section_name}, label_product__drug_label__source_product_number={source_product_id}, and label_product__drug_label__version_date={datetime.strptime(version, '%Y/%m/%d')} "
-                            )
+        if agency == "all":
+            sections = ProductSection.objects.all()
+        else:
+            sections = ProductSection.objects.filter(label_product__drug_label__source=agency).all()
+        self.total_sections = sections.count()
 
-        if vectorize:
-            if agency == "all":
-                sections = ProductSection.objects.all()
-            else:
-                sections = ProductSection.objects.filter(
-                    label_product__drug_label__source=agency
-                ).all()
-            self.total_sections = sections.count()
+        start = datetime.now()
+        loop = asyncio.get_event_loop()
+        looper = asyncio.gather(*[self.compute_section_vector_wrapper(s) for s in sections])  # type: ignore
+        results = loop.run_until_complete(looper)  # noqa F841
+        end = datetime.now()
+        elapsed = end - start
 
-            start = datetime.now()
-            loop = asyncio.get_event_loop()
-            looper = asyncio.gather(*[self.compute_section_vector_wrapper(s) for s in sections])  # type: ignore
-            results = loop.run_until_complete(looper)  # noqa F841
-            end = datetime.now()
-            elapsed = end - start
-
-            logger.info(
-                f"finished computing vectors ------------- { int(elapsed.total_seconds()) } seconds"
-            )
-            # logger.info(results)
+        logger.info(
+            f"finished computing vectors ------------- { int(elapsed.total_seconds()) } seconds"
+        )
+        # logger.info(results)
