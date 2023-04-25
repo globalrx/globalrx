d880457db9a76fd8036d69d3b74b213484203be8
aeg224@g.harvard.edu <aeg224@g.harvard.edu>
2023-04-24 21:33:47 -0400

    removed memory env var, set PROVISION_ES to false

--- aws_setup.sh
+++ aws_setup.sh
@@ -13,7 +13,6 @@ RDS_PW=`aws ssm get-parameters --names rds_pw --region us-east-1 | jq -r '.Param
 mv .env.example .env
 
 sed '/DATABASE.*/d' -i .env
-sed '/MEM_LIMIT.*/d' -i .env
 sed '/PROVISION_ES.*/d' -i .env
 sed '/ELASTICSEARCH_URL.*/d' -i .env
 
@@ -21,9 +20,5 @@ sed '/ELASTICSEARCH_URL.*/d' -i .env
 echo "DATABASE_URL=\"postgres://postgres:$RDS_PW@$DB_ENDPOINT:5432/postgres\"" >> .env
 echo "API_ENDPOINT=\"$EC2_IP\"" >> .env
 echo "ALLOWED_HOSTS=\"$EC2_IP, $ECS_LB\"">> .env
-echo "MEM_LIMIT=4294967296" >> .env
-echo "PROVISION_ES=True" >> .env
+echo "PROVISION_ES=False" >> .env
 echo "ELASTICSEARCH_URL=http://$EC2_IP:9200" >> .env
-
-echo "KIBANA_MEM_LIMIT=1073741824" >> .env
-
